19034d4d01341dbffd34410b1b1cc12853f026e5
zaqimaulana <zaqimaulana72@gmail.com>
2025-11-28 03:11:47 +0700

    menambahkan Margin, Shadow Color, Border Radius, dll

--- lib/pages/page_card_01.dart
+++ lib/pages/page_card_01.dart
@@ -13,7 +13,9 @@ class MyHomePage extends StatelessWidget {
             body: SingleChildScrollView(
               child: Container(
                 padding: const EdgeInsets.all(20.0),
+                child: Center(
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Card(
                     color: Colors.red,
@@ -57,10 +59,70 @@ class MyHomePage extends StatelessWidget {
                       child: Text("Shape Bingkai persegi panjang", style: TextStyle(fontSize: 12.0)),
                       ),
                       ),
+                      SizedBox(height: 20.0),
+
+                      Card(
+                      margin: EdgeInsets.all(16.0),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("Margin Card", style: TextStyle(fontSize: 10.0)),
+                      ),
+                      ),
+
+                      Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                        side: BorderSide(color: Colors.red, width: 2),
+                      ),
+                      borderOnForeground: true, // false
+                      child: Text(
+                        "Border tidak menimpa konten",
+                        style: TextStyle(fontSize: 10.0),
+                      ),
+                      // child: Padding(
+                      // padding: EdgeInsets.all(8),
+                      // child: Text("Border tidak menimpa konten"),
+                      // ),
+                      ),
+                      SizedBox(height: 20.0),
+
+                      Card(
+                      clipBehavior: Clip.antiAlias,
+                      child: Text("Anti alias clip Card", style: TextStyle(fontSize: 14.0)),
+                      ),
+                      SizedBox(height: 20.0),
+
+                      Card(
+                      semanticContainer: true,
+                      child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("Semantic true", style: TextStyle(fontSize: 14.0)),
+                      ),
+                      ),
+                      SizedBox(height: 20.0),
+
+                      Card(
+                      shadowColor: Colors.red,
+                      child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("Shadow Color Card", style: TextStyle(fontSize: 14.0)),
+                      ),
+                      ),
+                      SizedBox(height: 20.0),
+
+                      Card(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                      child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("Custom border Radius Card", style: TextStyle(fontSize: 14.0)),
+                      ),
+                      ),
+
                   ],
                 ),
               ),
             ),
-          );
+            ),
+            );
   }
 }
