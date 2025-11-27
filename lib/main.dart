d9c359d65d6b5561f5c77b308bc5fc21fda038df
zaqimaulana <zaqimaulana72@gmail.com>
2025-11-27 23:16:37 +0700

    membuat folder pages dan menambahkan isi kodingan pada page_card_01 agar terhubung dengan main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:week8_card/pages/page_card_01.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'card week 8',
-      home: const Scaffold(),
+      home: const MyHomePage(),
     );
   }
 }
---
+++ lib/pages/page_card_01.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+            appBar: AppBar(
+              title: const Text("Dashboard"),
+              actions: const [],
+            ),
+            body: SingleChildScrollView(
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                child: const Column(
+                  children: [],
+                ),
+              ),
+            ),
+          );
+  }
+}
